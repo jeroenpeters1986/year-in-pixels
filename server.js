f882d2f0c88a10c37b26e6305025ae179dcf076e
Jeroen Peters <jeroenpeters@impres.nl>
2024-01-07 15:30:46 +0100

    Node 20

--- server.js
+++ server.js
@@ -1,22 +1,23 @@
-var express = require('express');
-var app = express();
+const express = require('express');
+const app = express();
+require('dotenv').config()
 
 app.use(express.static('public'));
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
 
 function daysInMonth(month) {
-  var year = (new Date()).getFullYear();
+  let year = (new Date()).getFullYear();
   return new Date(year, month, 0).getDate();
 }
 
-var months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
-var grid = {};
-for (var i = 0; i < months.length; i++) {
+const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+let grid = {};
+for (let i = 0; i < months.length; i++) {
   grid[months[i]] = daysInMonth(i+1);
 }
 
-var moodOptions = {
+const moodOptions = {
   5: 'amazing',
   4: 'great',
   3: 'average',
@@ -29,6 +30,6 @@ app.get("/", function (req, res) {
   res.render('index', { grid: grid, moodOptions: moodOptions } );
 });
 
-var listener = app.listen('43751', function () {
+let listener = app.listen(process.env.PORT, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
